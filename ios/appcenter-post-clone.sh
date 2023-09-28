17cc958c1abad351d06b0468df014d104f3ee5b8
Volpe <renan.volpe@aluno.ifsp.edu.br>
2023-09-28 12:31:08 -0300

    hotfix3

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -8,7 +8,7 @@ set -x
 
 cd ..
 git clone -b test-ios-deploy https://github.com/renanvolpe/fakeStoreJoao.git 
-export PATH="C:/flutter/bin":$PATH
+export PATH="C:/flutter/bin"$PATH
 
 flutter channel stable
 flutter doctor
